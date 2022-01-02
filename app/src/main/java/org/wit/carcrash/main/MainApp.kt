8a51cacd4509a5280d1b1356805b70908b1cec98
EmmaKidney <ekidney@redhat.com>
2022-01-02 15:13:38 +0000

    new Converters helpers class

--- app/src/main/java/org/wit/carcrash/main/MainApp.kt
+++ app/src/main/java/org/wit/carcrash/main/MainApp.kt
@@ -3,6 +3,7 @@ package org.wit.carcrash.main
 import android.app.Application
 import org.wit.carcrash.models.CarCrashJSONStore
 import org.wit.carcrash.models.CarCrashStore
+import org.wit.carcrash.room.CarCrashStoreRoom
 import timber.log.Timber
 import timber.log.Timber.i
 
@@ -12,8 +13,7 @@ class MainApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
-        carcrashs = CarCrashJSONStore(applicationContext)
+        carcrashs = CarCrashStoreRoom(applicationContext)
         i("CarCrash started")
     }
 }
--- app/src/main/java/org/wit/carcrash/room/Database.kt
+++ app/src/main/java/org/wit/carcrash/room/Database.kt
@@ -2,9 +2,12 @@ package org.wit.carcrash.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import org.wit.carcrash.helpers.Converters
 import org.wit.carcrash.models.CarCrashModel
 
-@Database(entities = arrayOf(CarCrashModel::class), version = 1)
+@Database(entities = arrayOf(CarCrashModel::class), version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class Database : RoomDatabase() {
 
     abstract fun carcrashDao(): CarCrashDao
